977e51b6a3c0176e588b561e2e7116e76724315a
Alexander Luna <alexfox_2000@hotmail.com>
2023-02-12 10:33:07 -0500

    /api/whoami returns user's prefferred language and software

--- index.js
+++ index.js
@@ -20,7 +20,10 @@ app.get("/", function (req, res) {
 });
 
 app.get("/api/whoami", function (req, res) {
-  res.json({ ipaddress: req.socket.remoteAddress });
+  ipaddress = req.socket.remoteAddress;
+  language = req.headers["accept-language"];
+  software = req.headers["user-agent"];
+  res.json({ ipaddress, language, software });
 });
 
 // your first API endpoint...
